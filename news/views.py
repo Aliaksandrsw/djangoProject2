25e30477096ae16c7ef299306a48d4eed0268a0f
Aliaksandrsw <swordswird@proton.me>
2024-04-15 02:28:29 +0300

    Добавление связи ForeignKey, добавление html

--- news/views.py
+++ news/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
+
+from .models import News
 
 
-class NewsHome(View):
-    def get(self, request, **kwargs):
-        return HttpResponse('Хай')
+class NewsHome(ListView):
+    template_name = 'news/index.html'
+    context_object_name = 'news'
+
+    def get_queryset(self):
+        return News.objects.all()
