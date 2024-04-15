42e42ac242801fb2fea900bffe56827f8b137410
Aliaksandrsw <swordswird@proton.me>
2024-04-16 02:16:32 +0300

    Добавление категории новостей и параметров в url запросах

--- news/urls.py
+++ news/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.NewsHome.as_view()),
+    path('category<int:cat_id>/', views.NewsCategories.as_view(), name='news_category'),
+    path('category/<int:cat_id>/', views.NewsCategories.as_view(), name='news_category'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,14 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.views.generic import ListView
 
-from .models import News
+from .models import News, Category
 
 
 class NewsHome(ListView):
     template_name = 'news/index.html'
     context_object_name = 'news'
+    extra_context = {
+        'title': 'Список новостей',
+        'categories': Category.objects.all(),
+    }
 
     def get_queryset(self):
         return News.objects.all()
+
+
+class NewsCategories(ListView):
+    template_name = 'news/index.html'
+    context_object_name = 'news'
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['cat_id']).select_related('category')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cat_id'] = self.kwargs['cat_id']
+        return context
