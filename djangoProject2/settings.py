cdb0792577047a8b154bb44b1fb3118f7c90f8b9
Aliaksandrsw <swordswird@proton.me>
2024-04-18 00:05:46 +0300

    Добавление статики

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -119,6 +119,10 @@ FILE_CHARSET = 'utf-8'
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'djangoProject2/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
